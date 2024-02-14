7f39273503c31d03b63ace0bd5dd7dc22e6f667c
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2024-02-14 08:05:13 +0100

    task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,44 +1,50 @@
 // User controller module
+// import { ObjectId } from 'mongodb';
+import sha1 from 'sha1';
+import Queue from 'bull';
 import dbClient from '../utils/db';
+// import userUtils from '../utils/user';
 
-const sha1 = require('sha1');
+const userQueue = new Queue('userQueue');
 
 class UsersController {
   /**
    * creates a user using email and password
    */
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+  static async postNew(request, response) {
+    const { email, password } = request.body;
 
-    if (!email) {
-      return res.status(400).send({ error: 'Missing email' });
-    }
+    if (!email) return response.status(400).send({ error: 'Missing email' });
 
-    if (!password) {
-      return res.status(400).send({ error: 'Missing password' });
-    }
+    if (!password) { return response.status(400).send({ error: 'Missing password' }); }
 
     const existingUser = await dbClient.usersCollection.findOne({ email });
-    if (existingUser) {
-      return res.status(400).send({ error: 'Already exist' });
-    }
+
+    if (existingUser) { return response.status(400).send({ error: 'Already exist' }); }
 
     const hashedPassword = sha1(password);
 
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
-
     let result;
     try {
-      result = await dbClient.usersCollection.insertOne(newUser);
+      result = await dbClient.usersCollection.insertOne({
+        email,
+        password: hashedPassword,
+      });
     } catch (err) {
-      return res.status(500).send({ error: 'Error creating user.' });
+      await userQueue.add({});
+      return response.status(500).send({ error: 'Error creating user.' });
     }
 
-    const { insertedId } = result;
-    return res.status(201).send({ email, id: insertedId });
+    const user = {
+      id: result.insertedId,
+      email,
+    };
+
+    await userQueue.add({
+      userId: result.insertedId.toString(),
+    });
+
+    return response.status(201).send(user);
   }
 }
 
